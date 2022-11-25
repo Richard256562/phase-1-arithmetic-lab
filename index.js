ed4a478d3897b531482ed4072496bfaf5492ea5e
RichardOkongo <rirachard19@gmail.com>
2022-11-26 00:33:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+ 
+let num1 = 31;
+let num2 = 2;
+let multiply=num1 * num2;
+// random= math.random()
+let random=Math.floor(Math.random()) +1 
+let num3 = 5
+let num4 =19
 
-// Write your code here
+
+let mod= num4 % num3;
+console.log (mod)
+
+let max=Math.max
+console.log(max=(1,5,12,16,20))
+
+
